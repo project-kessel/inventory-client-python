7706217347c886241dd00bb0b3b6bbe424b2ff5d
Adam0Brien <aobrien@redhat.com>
2025-03-27 09:29:47 +0000

    update generate script and add example of client use

---
+++ example/main.py
@@ -0,0 +1,53 @@
+import grpc
+from kessel.inventory.v1beta2 import (
+    resource_pb2,
+    reporter_data_pb2,
+    resource_service_pb2_grpc,
+    resource_service_pb2
+)
+from google.protobuf import struct_pb2
+
+
+def run():
+    channel = grpc.insecure_channel("localhost:9000")
+    stub = resource_service_pb2_grpc.KesselResourceServiceStub(channel)
+
+    resource_data_struct = struct_pb2.Struct()
+
+    common_resource_data_struct = struct_pb2.Struct()
+    common_resource_data_struct.update({
+        "workspace_id": "123"
+    })
+
+    resource = resource_pb2.Resource(
+        resource_type="k8s_cluster",
+        reporter_data=reporter_data_pb2.ReporterData(
+            reporter_type="OCM",
+            reporter_instance_id="user@example.com",
+            reporter_version="0.1",
+            local_resource_id="2",
+            api_href="www.example.com",
+            console_href="www.rhel.example.com",
+            resource_data=resource_data_struct
+        ),
+        common_resource_data=common_resource_data_struct
+    )
+
+    # Wrap in ReportResourceRequest
+    request = resource_service_pb2.ReportResourceRequest(
+        resource=resource
+    )
+
+    # Send gRPC request
+    try:
+        response = stub.ReportResource(request)
+        print("Resource reported successfully")
+        print(response)
+    except grpc.RpcError as e:
+        print("gRPC error occurred:")
+        print(f"Code: {e.code()}")
+        print(f"Details: {e.details()}")
+
+
+if __name__ == "__main__":
+    run()
--- generate_python_grpc_client.sh
+++ generate_python_grpc_client.sh
@@ -2,34 +2,26 @@
 
 set -e
 
-GITHUB_REPO_URL="https://github.com/project-kessel/inventory-api"
-PROTO_FOLDER="api/kessel/inventory/v1beta1"
-TEMP_DIR=$(mktemp -d)
-VENV_DIR="$TEMP_DIR/venv"
-OUTPUT_DIR=$(pwd)
-
 echo "Creating and activating the virtual environment"
- python3 -m venv venv
- source venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 
- echo "Installing the inventory api python client"
- python3 -m pip install project-kessel-inventory-api-grpc-python --extra-index-url https://buf.build/gen/python
-echo "Copying files to necessary directory for buf validation files"
- mkdir -p src/buf/validate
- cp -r venv/lib/python*/site-packages/buf/validate/* src/buf/validate
+echo "Installing the inventory api python client"
+python3 -m pip install project-kessel-inventory-api-grpc-python --extra-index-url https://buf.build/gen/python
 
-echo "Copying files to necessary directory for buf validation files"
+echo "Installing package locally"
+pip install .
+
+echo "Copying files to src directory for buf validation"
 mkdir -p src/buf/validate
 touch src/buf/__init__.py
 touch src/buf/validate/__init__.py
 cp -r venv/lib/python*/site-packages/buf/validate/* src/buf/validate
 
-echo "Copying files to necessary directory"
-mkdir -p src/kessel/inventory/v1beta1
+echo "Copying files to src directory"
 cp -r venv/lib/python*/site-packages/kessel/inventory/v1/* src/kessel/inventory/v1/
 cp -r venv/lib/python*/site-packages/kessel/inventory/v1beta1/* src/kessel/inventory/v1beta1/
 cp -r venv/lib/python*/site-packages/kessel/inventory/v1beta2/* src/kessel/inventory/v1beta2/
-
 
 echo "Deactivating and removing the virtual environment..."
 deactivate
@@ -37,5 +29,4 @@ deactivate
 echo "Removing the virtual environment"
 rm -rf "venv"
 
-
-echo "python client successfully updated"
+echo "Python client successfully updated"
